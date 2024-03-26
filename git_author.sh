a6421b1c819491bb26185b3802bd331ecc70f6a9
admiral9200 <lion9200.world@gmail.com>
2024-03-26 14:14:59 +0800

    11

--- git_author.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter '
-	CORRECT_NAME="admiral9200"
-	CORRECT_EMAIL="lion9200.world@gmail.com"
-	    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-	    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-	    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-	    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-	' -f --tag-name-filter cat -- --branches --tags
